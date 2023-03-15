d8051beec891d872db1b5ad3383607613423d18e
Hikosso <onikmarcin@wp.pl>
2023-03-15 11:48:30 +0100

    dodanie api, okHTTP3

--- Target App/app/src/main/java/com/example/recipesapp/mainMenu/MainMenuActivity.java
+++ Target App/app/src/main/java/com/example/recipesapp/mainMenu/MainMenuActivity.java
@@ -2,15 +2,28 @@ package com.example.recipesapp.mainMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.recipesapp.R;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main_menu);
+
+
     }
+
+
 }
