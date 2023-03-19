b7948f4f3b975fa9f70f3c3b217e2797692c32e8
Kajetan Nowak <kajtek2n@gmail.com>
2023-03-19 13:15:21 +0100

    Create fridge activity, product adapter

--- Target App/app/src/main/java/com/example/recipesapp/Libraries/Product.java
+++ Target App/app/src/main/java/com/example/recipesapp/Libraries/Product.java
@@ -1,4 +1,28 @@
 package com.example.recipesapp.Libraries;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Product {
+    private String name;
+    private String expirationDate;
+    private SimpleDateFormat dateFormat = (SimpleDateFormat) SimpleDateFormat.getDateInstance();
+
+
+    public Product(String name, String expirationDate) {
+        this.name = name;
+        this.expirationDate = expirationDate;
+
+
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
 }
--- Target App/app/src/main/java/com/example/recipesapp/fridge/FridgeActivity.java
+++ Target App/app/src/main/java/com/example/recipesapp/fridge/FridgeActivity.java
@@ -1,16 +1,46 @@
 package com.example.recipesapp.fridge;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.recipesapp.Libraries.Product;
+import com.example.recipesapp.Libraries.ProductAdapter;
 import com.example.recipesapp.R;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class FridgeActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ProductAdapter productAdapter;
+    private ArrayList<Product> products = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fridge);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        productAdapter = new ProductAdapter(this, products);
+        recyclerView.setAdapter(productAdapter);
+
+        products.add(new Product("Mleko", "2023-09-1"));
+        products.add(new Product("Kawa","2025-4-6"));
+        products.add(new Product("Chleb","1999-5-15"));
+        products.add(new Product("Wiśnie","1999-5-15"));
+        products.add(new Product("Przyprawy","1999-5-15"));
+        products.add(new Product("Woda","1999-5-15"));
+        products.add(new Product("Jack Daniel's","1999-5-15"));
+
+
+        productAdapter.notifyDataSetChanged();
+
     }
+
+
 }
